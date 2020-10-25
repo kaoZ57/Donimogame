09409fc247c1d1e049f674c032afd0f8f8e03483
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2020-10-25 19:51:10 +0700

    Update Board.cs

--- DominnoGame/Board.cs
+++ DominnoGame/Board.cs
@@ -9,11 +9,17 @@ namespace DominnoGame
     {
         private readonly List<Player> players;
         private List<Domino> board = new List<Domino>();
-        Player player = new Player();
         Deck deck = new Deck();
-        int _CountDominoDeck = 28;
+        private Player dealer;
         int Round = 1;
         //ConsoleKeyInfo chinput = Console.ReadKey();
+
+        public Board(string dealerName)
+        {
+            this.dealer = new Player(dealerName);
+            players = new List<Player>();
+
+        }
         public void AddPlayer(Player player)
         {
             players.Add(player);
@@ -25,26 +31,31 @@ namespace DominnoGame
         }
         private void initialCard()
         {
+
             for (int i = 0; i < 5; i++)
             {
-                player.GetDomino(deck.Deal());
-                _CountDominoDeck -= 1;
+                foreach (var P in players)
+                {
+                    P.GetDomino(deck.Deal());
+                }
+                dealer.GetDomino(deck.Deal());
             }
-        }    
+        }
 
         public void battle()
         {
         UP:
             Console.Clear();
-            
-            if (_CountDominoDeck == 0)
+
+            if (deck.Count == 0)
             {
                 result();
             }
             else
             {
                 Console.WriteLine("***********************");
-                Console.WriteLine("| Number of domino {0} |", _CountDominoDeck);
+                Console.WriteLine("| Round {0}             |", Round);
+                Console.WriteLine("| Number of domino {0} |", deck.Count);
                 Console.WriteLine("***********************");
                 Console.Write("Board|");
                 foreach (var d in board)
@@ -53,45 +64,70 @@ namespace DominnoGame
                     Console.Write("|");
                 }
                 Console.WriteLine("");
-                player.Show();
-                Connect_domino();               
+               
+
+                players[0].Show();
+
+                /*foreach (var P in players)
+                {
+                    P.Show();
+                }*/
+                Connect_domino();
+                Round = Round + 1;
                 goto UP;
-            }           
+            }
         }
 
         public void Connect_domino()
         {
-            int Selection_number;
-        UP:
-            Console.Write("SelectionDomino = ");
-            string InputNum = Console.ReadLine();
-            try
+        /*foreach (var P in players)
+        {
+            Domino d = P.SelectDomino();
+            board.Add(P.DropDomino(d));
+        }*/
+        if (Round == 1)
             {
-                Selection_number = Int32.Parse(InputNum);
-            }
-            catch (Exception)
-            {
-                goto UP;
-            }
-            if(Selection_number > player.dominoslist.Count)
-            {
-                goto UP;
-            }
+                Domino d = players[0].SelectDomino();
+                board.Add(players[0].DropDomino(d));
 
-            if(Round == 1)
-            {
-                board.Add(player.Move_domino(Selection_number));
+                Domino D = dealer.AILogic();
+                board.Add(dealer.DropDomino(D));
             }
-            else
+        else
             {
-                //player.dominoslist[Selection_number - 1];
-                board.Add(player.Move_domino(Selection_number));
-            }
+            Up:
+                Console.Write("GetDonimo? (y/n): ");
+                string input = Console.ReadLine().ToLower();
+                switch (input)
+                {
+                    case "y":
+                        players[0].GetDomino(deck.Deal());
+                        break;
+                    case "n":
+                        Domino d = players[0].SelectDomino();
+                        board.Add(players[0].DropDomino(d));
+
+                        Domino D = dealer.AILogic();
+                        board.Add(dealer.DropDomino(D));
+                        break;
+                    default:
+                        goto Up;
+                }
+            }      
         }
-      
+
         public void result()
         {
             Console.WriteLine("End");
+        }
+
+        public Domino FirstCheckHand()
+        {
+            return board[0];
+        }
+        public Domino LastCheckHand()
+        {
+            return board[board.Count];
         }
     }
 }
