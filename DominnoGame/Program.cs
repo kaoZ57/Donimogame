6099fd882e87f814b0b6dc26b75eee919380dd7f
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2020-10-25 19:51:27 +0700

    Update Program.cs

--- DominnoGame/Program.cs
+++ DominnoGame/Program.cs
@@ -6,18 +6,39 @@ namespace DominnoGame
     {
         static void Main(string[] args)
         {
+
             //Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Board board = new Board();
+            Board board = new Board("dealer");
+            //Board board = new Board();
 
             /*Console.Write("Enter Name Here = ");
-            string addname = Console.ReadLine();
-        
-            Player player1 = new Player(addname);       
-            board.AddPlayer(player1);*/
+            string addname = Console.ReadLine();*/
+
+            board.AddPlayer(new Player("P1"));
 
             board.Play();
 
+
+            //testDropDomono();
             Console.ReadKey();
+        }
+
+        static void testDropDomono()
+        {
+            Player p = new Player("A");
+            Deck deck = new Deck();
+            p.GetDomino(deck.Deal());
+            p.GetDomino(deck.Deal());
+            p.GetDomino(deck.Deal());
+
+            p.Show();
+
+            Domino d = p.SelectDomino();
+
+            Console.WriteLine(d.ToString());
+
+            p.DropDomino(d);
+            p.Show();
         }
     }
 }
